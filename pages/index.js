d3f8e155e295b2cd8d67f1c2ea68ee1ed10e11bf
Anish <helloanishjain@gmail.com>
2022-09-05 13:33:36 +0530

    add stream network

--- pages/index.js
+++ pages/index.js
@@ -20,55 +20,8 @@ export default function Home() {
   //     setLoading(false);
   //   });
   // }, []);
-
-  const streamrRef = useRef();
-  useEffect(() => {
-    streamrRef.current = new StreamrClient({
-      auth: {
-        ethereum: window.ethereum,
-      },
-    });
-
-    const msg = {
-      temperature: 25.4,
-      humidity: 10,
-      happy: true,
-    };
-
-    const subscription = streamrRef.current
-      .subscribe("0x2ea3bf6b653375fb8facfb67f19937e46840a7d4/lands/")
-      .then((content, metadata) => {
-        console.log({ content });
-        console.log({ metadata });
-        streamrRef.current
-          .publish("0x2eA3bF6B653375fb8facfB67F19937E46840a7d4/lands/", msg)
-          .then((data) => {
-            console.log("DATA", data);
-          });
-      });
-
-    // const stream = streamrRef.current
-    //   .getOrCreateStream({
-    //     id: "0x2ea3bf6b653375fb8facfb67f19937e46840a7d4/lands/",
-    //   })
-    //   .then((res) => {
-    //     res.getPermissions().then((p) => console.log(p));
-    //     console.log("get created", res);
-    //     res.grantPermissions({
-    //       public: true,
-    //       permissions: [
-    //         StreamPermission.PUBLISH,
-    //         StreamPermission.SUBSCRIBE,
-    //         StreamPermission.EDIT,
-    //         StreamPermission.GRANT,
-    //         StreamPermission.DELETE,
-    //       ],
-    //     });
-    //   });
-  }, []);
-
-  // console.log(streamrRef.current);
-
+  
+  
   return (
     <div className="">
       {/* <div className="absolute top-3 right-6">
---
+++ pages/streamNetwork.js
@@ -0,0 +1,37 @@
+import { useEffect, useRef } from "react";
+
+const STREAM_ID = "0x2ea3bf6b653375fb8facfb67f19937e46840a7d4/lands/";
+
+export const StreamNetwork = () => {
+  const streamrRef = useRef();
+
+  useEffect(() => {
+    streamrRef.current = new StreamrClient({
+      auth: {
+        ethereum: window.ethereum,
+      },
+    });
+
+    streamrRef.current.getOrCreateStream({ id: "/lands/" }).then((res) => {
+      res.getPermissions().then((p) => console.log(p));
+      res.grantPermissions({
+        public: true,
+        permissions: [StreamPermission.PUBLISH, StreamPermission.SUBSCRIBE],
+      });
+    });
+
+    const msg = {
+      temperature: 25.4,
+      humidity: 10,
+      happy: true,
+    };
+
+    streamrRef.current.subscribe(STREAM_ID).then((content, metadata) => {
+      streamrRef.current.publish(STREAM_ID, msg).then((data) => {
+        console.log({ data });
+      });
+    });
+  }, []);
+
+  return <div>Stream Network</div>;
+};
